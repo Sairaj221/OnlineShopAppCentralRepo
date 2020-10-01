e2fae9423be89c0109d981a0f4cf3d78b1da2ce4
Sairaj221 <govindakhil123@gmail.com>
2020-10-01 14:05:19 +0530

    2 new points are added

--- OnlineShopApp/src/main/java/com/cg/onlineshop/controllers/ProductController.java
+++ OnlineShopApp/src/main/java/com/cg/onlineshop/controllers/ProductController.java
@@ -1,19 +1,32 @@
 package com.cg.onlineshop.controllers;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cg.onlineshop.beans.Product;
+
 @RestController
 public class ProductController {
 
 	@GetMapping("/hello")
-	public ResponseEntity<String> sayHello(){
-		return new ResponseEntity<>("Hello To All Welcome to OnlineShop REST Services",HttpStatus.OK);
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<>("Hello To All Welcome to OnlineShop REST Services", HttpStatus.OK);
 	}
+
+	@GetMapping("/hi")
+	public ResponseEntity<String> sayHi() {
+		return new ResponseEntity<>("Hi To All Welcome to OnlineShop REST Services", HttpStatus.OK);
+	}
+
+	@GetMapping("/bye")
+	public ResponseEntity<String> sayGoodbye() {
+		return new ResponseEntity<>("Goodbye for the day", HttpStatus.OK);
+	}
+
 	@GetMapping("/getProduct")
-	public ResponseEntity<Product> getProduct(){
+	public ResponseEntity<Product> getProduct() {
 		Product product = new Product(111, 150000, "HP Laptop");
-		return new ResponseEntity<>(product,HttpStatus.OK);
+		return new ResponseEntity<>(product, HttpStatus.OK);
 	}
 }
